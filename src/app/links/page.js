cc7e078d1d35b6ffb55b46c66246891ce4cd181c
Md Zahed Hossain <3605242+mdzahedhossain@users.noreply.github.com>
2024-07-23 22:56:20 -0400

    adding links create form

---
+++ src/app/links/page.js
@@ -0,0 +1,10 @@
+import LinksCreateForm from "./createForm";
+
+
+export default function LinksPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <LinksCreateForm />
+    </main>
+  );
+}
