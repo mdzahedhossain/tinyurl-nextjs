15d70534daeb0acced92703bbffe29ea959bf1cc
Md Zahed Hossain <3605242+mdzahedhossain@users.noreply.github.com>
2024-07-26 20:55:10 -0400

    fix logic again

--- src/lib/getDomain.js
+++ src/lib/getDomain.js
@@ -1,6 +1,6 @@
 const getDomain = () => {
     const protocol = process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? "https" : "http";
-    const domain = process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? process.env.NEXT_PUBLIC_VERCEL_URL : "localhost:3000";
+    const domain = process.env.NEXT_PUBLIC_VERCEL_URL ? process.env.NEXT_PUBLIC_VERCEL_URL : "localhost:3000";
     
     const url = `${protocol}://${domain}`;
     return url;
